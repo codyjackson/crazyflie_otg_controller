8ea17587c2c38a194bbe016eceb36402399c3a05
Cody Jackson <sxenog@gmail.com>
2014-09-01 22:16:06 -0700

    Connect now has some directions and when tapping the screen will redirect the user to calibrate. Calibrate now has the infographic and some directions and when tapping the screen it will take the user to fly. Created track and lever directive so that I could make a thrust lever in the fly page. Created a mock radio service.

--- ui/assets/javascripts/main.js
+++ ui/assets/javascripts/main.js
@@ -22,9 +22,28 @@ angular.module('application', ['ngRoute'])
                 controller: 'ReconnectController'   
             });
     }])
-    .directive('bar', [function(){
+    .factory('radio', [function(){
         return {
-            restrict: 'E',
+            connect: function() {
+
+            },
+            setTargetThrust: function(percentage){
+                console.log(percentage);
+            },
+            setTargetPitch: function(degrees){
+
+            },
+            setTargetRoll: function(degrees) {
+
+            },
+            setTargetYaw: function(degrees) {
+
+            }
+        };
+    }])
+    .directive('bar', [function(){
+        return { 
+            restrict: 'A',
             templateUrl: '../partial-views/bar.html',
             transclude: true
         };
@@ -53,20 +72,124 @@ angular.module('application', ['ngRoute'])
             }
         };
     }])
+    .directive('track', ['$parse', function($parse){
+        var _leverElement = null;
+        var _moving = false;
+
+        function contains(str, substr) {
+            return str.indexOf(substr) > -1;
+        }
+
+        function extractX(ev) {
+            ev = ev.originalEvent;
+            if(contains(ev.type, 'mouse')) {
+                return ev.clientX;
+            }
+            if(ev.targetTouches.length != 1) {
+                return;
+            }
+
+            return ev.targetTouches[0].clientX;
+        }
+
+        function killPropegation(ev) {
+            ev.stopImmediatePropagation()
+        }
+
+        function fit(min, max, x) {
+            if(x < min)
+                return min;
+            if(x > max)
+                return max;
+            return x;
+        }
+
+        return {
+            restrict: 'A',
+            controller: function(){
+                this.registerLeverElement = function(leverElement){
+                    _leverElement = $(leverElement);
+                };
+            },
+            link: function(scope, element, attributes){
+                var updatePercentage = function(){
+                    var fn = $parse(attributes.track);
+                    return function(percentage){ 
+                        scope.$apply(function(){
+                            fn(scope, {$percentage: percentage});
+                        });
+                    };
+                }();
+
+                function updateLever(ev) {
+                    if(contains(ev.type, 'down') || contains(ev.type, 'start')) {
+                        _moving = true;
+                    }
+                    if(contains(ev.type, 'up') || contains(ev.type, 'end')) {
+                        _moving = false;
+                    }
+
+                    if(!_moving) {
+                        return;
+                    }
+
+                    var inputX = extractX(ev);
+                    if(!inputX) {
+                        return;
+                    }
+
+                    var leverWidth = _leverElement.outerWidth();
+                    var leverHalfWidth = leverWidth / 2;
+
+                    var min = 0;
+                    var max = element.outerWidth() - leverWidth;
+                    var left = fit(min, max, inputX - leverHalfWidth);
+
+                    _leverElement.css({left: left});
+                    updatePercentage(left/max);
+                }
+
+                element.on('mousedown', updateLever);
+                element.on('mouseup', updateLever);
+                element.on('mousemove', updateLever);
+
+                element.on('touchcancel', killPropegation);
+                element.on('touchstart', updateLever);
+                element.on('touchend', updateLever);
+                element.on('touchmove', updateLever);
+            }
+        };
+    }])
+    .directive('lever', [function(){
+        return {
+            restrict: 'A',
+            require: '^track',
+            link: function(scope, element, attributes, trackControl) {
+                trackControl.registerLeverElement(element);
+            }
+        };
+    }])
     .controller('GlobalController', ['$scope', '$rootScope', function($scope, $rootScope){
         $scope.onScreenTap = function(){
             $rootScope.$emit('SCREEN_TAP');
         };
     }])
-    .controller('ConnectController', [function(){
-        console.log('ConnectController');
-    }])
-    .controller('CalibrateController', ['$scope', '$rootScope', function($scope, $rootScope){
+    .controller('ConnectController', ['$rootScope', '$location', function($rootScope, $location){
         $rootScope.$on('SCREEN_TAP', function(){
+            $rootScope.phoneOrientation = 0;
+            $location.path('calibrate');
         });
     }])
-    .controller('FlyController', [function(){
-        console.log('FlyController');
+    .controller('CalibrateController', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location){
+        $rootScope.$on('SCREEN_TAP', function(){
+            $rootScope.phoneOrientation = 0;
+            $location.path('fly');
+        });
+    }])
+    .controller('FlyController', ['$rootScope', '$scope', 'radio' function($rootScope, $scope, radio){
+        $scope.onNewThrust = function(percentage) {
+            radio.setTargetThrust(percentage);
+        }
     }])
     .controller('ReconnectController', [function(){
         console.log('ReconnectController');
